654fd9274dc1ac0eafd1b346c013a8404212a22d
snewhouse <stephen.j.newhouse@gmail.com>
2016-05-31 14:43:52 +0000

    almost there

--- sjn/start_neo4j_npm.sh
+++ sjn/start_neo4j_npm.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/env bash
-cd /var/lib/neo4j/ && \
-  /bin/bash /docker-entrypoint.sh neo4j | tee -a /sracth/start.log & \
-  sleep 5s && \
-  cd /usr/src/app && \
-  npm start | tee -a /sracth/start.log && \
-  sleep 1s
+
+RUNDATE=`date +"%d%m%y"`
+DATEHASH=`date | md5sum | cut -c 1-6`
+
+cd /usr/src/app
+nohup npm start > /scratch/start.npm.${RUNDATE}.${DATEHASH}.log &
+
+cd /var/lib/neo4j
+nohup /bin/bash /docker-entrypoint.sh neo4j > /scratch/start.neo4j.${RUNDATE}.${DATEHASH}.log &
+
